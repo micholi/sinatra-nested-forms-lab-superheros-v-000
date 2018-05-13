a077bcdf90adf9bb7d78ddb6dca834f06bcc3521
Michelle Olivieri <michelleo72@gmail.com>
2018-05-13 23:00:32 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,7 @@ class App < Sinatra::Base
     end
 
     post '/teams' do
+      binding.pry
       @team = Team.new(params[:team])
       params[:team][:superheroes].each do |details|
         Superhero.new(details)
